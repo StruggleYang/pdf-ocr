48bbf907664ac6e52285db8b5e783879b48e65af
struy <1724555319@qq.com>
2022-03-03 20:24:40 +0800

    test

--- test/test.py
+++ test/test.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
 # coding:utf-8
+import time
 import pdfplumber
 import os
-from util.log import logger
+# from util.log import logger
 
 if __name__ == '__main__':
+    print("hello")
     user_path = os.path.expanduser('~')
+    print(user_path)
     with pdfplumber.open(os.path.join(user_path, "working", "pdftest", "test.pdf")) as pdf:
         for page in pdf.pages:
-            # 默认解析前5页
-            if page.page_number > 5:
-                continue
             all_content = page.extract_text(x_tolerance=0, y_tolerance=0)
-            all_content = all_content.split("\n")
             tables = page.extract_table()
-            # img = page.to_image()
-            # img.draw_rects(page.extract_words())
-            # img.save('解析page-%s.png' % page.page_number, format="PNG")
-            logger.info(page.page_number)
-            logger.info(tables)
-            logger.info(all_content)
+            print(page.page_number)
+            print(tables)
+            print(all_content)
+    print("end")
+    time.sleep(10)
